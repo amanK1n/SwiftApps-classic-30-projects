cf6db70727c203d60449722e9a9fdd992ae1a3d0
Sayed Aman Konen <sayed.konen@comviva.com>
2025-07-20 11:07:20 +0530

    Added guess the flag game

---
+++ GuessTheFlag2/GuessTheFlag2Tests/GuessTheFlag2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlag2Tests.swift
+//  GuessTheFlag2Tests
+//
+//  Created by Sayed on 20/07/25.
+//
+
+import Testing
+@testable import GuessTheFlag2
+
+struct GuessTheFlag2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
