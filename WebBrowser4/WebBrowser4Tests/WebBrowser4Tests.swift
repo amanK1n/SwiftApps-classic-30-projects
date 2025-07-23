4866b2d9d8675788782eab728eddcb7efad9605c
Sayed Aman Konen <sayed.konen@comviva.com>
2025-07-23 09:40:53 +0530

    Added web browser app

---
+++ WebBrowser4/WebBrowser4Tests/WebBrowser4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  WebBrowser4Tests.swift
+//  WebBrowser4Tests
+//
+//  Created by Sayed on 20/07/25.
+//
+
+import Testing
+@testable import WebBrowser4
+
+struct WebBrowser4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
