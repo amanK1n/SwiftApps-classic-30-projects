fe3128de5436425beb2ebbbfc9f61ac933456c64
Sayed Aman Konen <amankonen0799@gmail.com>
2025-07-27 13:44:12 +0530

    Added word scramble app

---
+++ WordScramble5/WordScramble5Tests/WordScramble5Tests.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramble5Tests.swift
+//  WordScramble5Tests
+//
+//  Created by Sayed on 25/07/25.
+//
+
+import Testing
+@testable import WordScramble5
+
+struct WordScramble5Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
