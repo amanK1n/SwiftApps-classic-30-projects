b2064df302dbabbfffa97d3225bb4373816704bd
Sayed Aman Konen <amankonen0799@gmail.com>
2025-07-30 00:51:16 +0530

    Added news app

---
+++ 7NewsApp/7NewsAppTests/_NewsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  _NewsAppTests.swift
+//  7NewsAppTests
+//
+//  Created by Sayed on 29/07/25.
+//
+
+import Testing
+@testable import _NewsApp
+
+struct _NewsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
