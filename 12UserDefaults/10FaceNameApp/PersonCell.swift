6a351fd4727eb9508d6f943fea9637c12f9acdc1
Sayed Aman Konen <amankonen0799@gmail.com>
2025-08-05 00:18:44 +0530

    Added code for userDefaults NSCoding

---
+++ 12UserDefaults/10FaceNameApp/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  10FaceNameApp
+//
+//  Created by Sayed on 03/08/25.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet public var imageView: UIImageView?
+    @IBOutlet public var name: UILabel?
+    
+    
+}
