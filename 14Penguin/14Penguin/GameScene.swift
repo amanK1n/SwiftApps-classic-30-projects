8c45bbbba0bf12403863d39134c7d94f4a67cea2
Sayed Aman Konen <amankonen0799@gmail.com>
2025-08-07 09:17:32 +0530

    Added initial code to 14Penguin

---
+++ 14Penguin/14Penguin/GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  14Penguin
+//
+//  Created by Sayed on 07/08/25.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+  
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    
+}
