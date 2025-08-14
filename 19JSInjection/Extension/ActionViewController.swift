b3168e76ddf3b2edc5675d11d90bf144304a4633
Sayed Aman Konen <amankonen0799@gmail.com>
2025-08-15 00:06:24 +0530

    Added keeb height proj19

--- 19JSInjection/Extension/ActionViewController.swift
+++ 19JSInjection/Extension/ActionViewController.swift
@@ -17,6 +17,13 @@ class ActionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeeb), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeeb), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
+        
         if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
             if let itemProvider = inputItem.attachments?.first {
                 itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) {
@@ -34,7 +41,24 @@ class ActionViewController: UIViewController {
         }
            
     }
-
+    @objc func adjustForKeeb(notification: Notification) {
+        guard let keyBoardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keebScreenEndFrame = keyBoardValue.cgRectValue
+        
+        let keebHeight = view.convert(keebScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            script.contentInset = .zero
+        } else {
+            script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keebHeight.height - view.safeAreaInsets.bottom, right: 0)
+        }
+        
+        script.scrollIndicatorInsets = script.contentInset
+        let selection = script.selectedRange
+        script.scrollRangeToVisible(selection)
+        
+    }
     @IBAction func done() {
         let item = NSExtensionItem()
         let argument: NSDictionary = ["customJavaScript": script.text]
