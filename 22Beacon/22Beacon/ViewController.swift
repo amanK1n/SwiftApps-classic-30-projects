af52309c75c2b49a36c4f917a98b3e3022fa87be
Sayed Aman Konen <amankonen0799@gmail.com>
2025-08-19 21:39:54 +0530

    Added project 22Beacon

---
+++ 22Beacon/22Beacon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  22Beacon
+//
+//  Created by Sayed on 19/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
