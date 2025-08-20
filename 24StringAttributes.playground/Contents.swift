79a94f622e676f260a7d7431d516ae2d3f746e29
Sayed Aman Konen <amankonen0799@gmail.com>
2025-08-20 20:45:26 +0530

    Added string attribute playground

--- 24StringAttributes.playground/Contents.swift
+++ 24StringAttributes.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let name = "Aman"
+
+let letter = name[name.index(name.startIndex, offsetBy: 1)]
+print(letter)
+
+
+extension String {
+    subscript(i: Int) -> String {
+        return String(self[self.index(self.startIndex, offsetBy: i)])
+    }
+    
+}
+let l2 = name[3]
+print(l2)
+
+
+for letter in name {
+   print("Gimme a \(letter)!"
+}
+
+
+print("What does that spell? \(name.uppercased())!")
