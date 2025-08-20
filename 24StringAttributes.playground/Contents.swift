45908c5f49ee39bf17785b5de1ae8aeabb664696
Sayed Aman Konen <amankonen0799@gmail.com>
2025-08-20 23:46:36 +0530

    Added string attribute playground

--- 24StringAttributes.playground/Contents.swift
+++ 24StringAttributes.playground/Contents.swift
@@ -1,5 +1,5 @@
 import UIKit
-
+// PART-1
 let name = "Aman"
 
 let letter = name[name.index(name.startIndex, offsetBy: 1)]
@@ -16,9 +16,57 @@ let l2 = name[3]
 print(l2)
 
 
-for letter in name {
-   print("Gimme a \(letter)!"
+//for letter in name {
+//   print("Gimme a \(letter)!"
+//}
+
+// PART-2
+let password = "12345"
+password.hasPrefix("123")
+password.hasSuffix("345")
+
+extension String {
+    func deletingPrefix(_ prefix: String) -> String {
+        guard self.hasPrefix(prefix) else { return self }
+            return String(self.dropFirst(prefix.count))
+        
+    }
+    func deletingSuffix(_ suffix: String) -> String {
+        guard self.hasSuffix(suffix) else { return self }
+        return String(self.dropLast(suffix.count))
+    }
 }
+let x = password.deletingPrefix("12")
+print(x)
+let y = password.deletingSuffix("45")
+print(y)
 
+extension String {
+    var capitalizedFirstLetter: String {
+        guard let firstLetter = self.first else { return self }
+        return firstLetter.uppercased() + self.dropFirst()
+    }
+}
+let greeting = "hello hoho shgsdh kgjkg"
+let capitalizedGreeting = greeting.capitalizedFirstLetter
+print(capitalizedGreeting)
 
-print("What does that spell? \(name.uppercased())!")
+let input = "Swift is like Objective-C without the C"
+input.contains("Swift")
+
+let languages = ["Python", "Ruby", "Swift"]
+languages.contains("Swift")
+
+extension String {
+    func isMyWordPresent(array: [String]) -> Bool {
+        for word in array {
+            if self.contains(word) {
+                return true
+            }
+        }
+        return false
+    }
+}
+let a = languages.isMyWordPresent(array: languages)
+print(a)
+
